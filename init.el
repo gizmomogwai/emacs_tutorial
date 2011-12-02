f1daeb34240176f4cb38b3fdd9fd41b66aa3bd89
Christian Köstlin <christian.koestlin@gmail.com>
2011-12-02 09:58:19 +0100

    added two basic tests

--- init.el
+++ init.el
@@ -1,2 +1,9 @@
 (add-to-list 'load-path (file-name-directory load-file-name))
 (require 'el-expectations)
+
+(expectations
+  (desc "good test")
+  (expect 1 1)
+  (desc "bad test")
+  (expect 1 2)
+  )
