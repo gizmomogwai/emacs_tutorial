2e45c52626c82df2ec5f291731a56d6264b2f6d3
Christian Köstlin <christian.koestlin@gmail.com>
2011-12-11 01:05:24 +0100

    user-callable function

--- init.el
+++ init.el
@@ -27,6 +27,14 @@
   "replaces the file-extension and the regexp-patterns"
   (tff-replace-with-first-matching-regexp regexp-patterns (or (tff-replace-extension ext-patterns input) input)))
 
+(defun tff
+  ()
+  "toggles between friend fiels (see tff customization group)"
+  (interactive)
+  (let* ((file-name (buffer-file-name))
+         (new-file-name (tff-calc-file-name '(("cpp" "h")("h" "cpp")) '() file-name)))
+    (if (not (string= file-name new-file-name)) (find-file new-file-name))))
+
 (expectations
   (desc "nil when no matching extension")
   (expect nil (tff-replace-extension '(("cpp" "h")) "test.rb"))
