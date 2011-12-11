0939909e08982508fca0c51700025a5e90eccfd8
Christian Köstlin <christian.koestlin@gmail.com>
2011-12-11 01:14:39 +0100

    first real tests running

--- init.el
+++ init.el
@@ -1,9 +1,18 @@
 (add-to-list 'load-path (file-name-directory load-file-name))
 (require 'el-expectations)
 
+(defun tff-replace-extension
+  (patterns input)
+  "replaces the extension from input with a matching pattern from patterns"
+  (let*
+      ((extension (file-name-extension input))
+       (basename (file-name-sans-extension input))
+       (p (assoc extension patterns)))
+    (if p (concat basename "." (car (cdr p))) nil)))
+
 (expectations
-  (desc "good test")
-  (expect 1 1)
-  (desc "bad test")
-  (expect 1 2)
+  (desc "nil when no matching extension")
+  (expect nil (tff-replace-extension '(("cpp" "h")) "test.rb"))
+  (desc "not nil when a extension matches")
+  (expect "test.yaml" (tff-replace-extension '(("cpp" "h")("rb" "yaml")) "test.rb"))
   )
