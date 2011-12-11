e4eccc0791e725bae2dcb4b518ecd18e509cc152
Christian Köstlin <christian.koestlin@gmail.com>
2011-12-11 01:15:20 +0100

    added replace-with-first-matching-regexp

--- init.el
+++ init.el
@@ -10,9 +10,26 @@
        (p (assoc extension patterns)))
     (if p (concat basename "." (car (cdr p))) nil)))
 
+(defun tff-replace-with-first-matching-regexp
+  (patterns input)
+  "iterates over patterns and return the regexp-replace of the first regexp-match"
+  (if (first patterns)
+      (let* ((pair (first patterns))
+             (pattern (car pair))
+             (repl (car (cdr pair)))
+             (replaced (replace-regexp-in-string pattern repl input))
+             (finished (not (string= replaced input))))
+        (if finished replaced (tff-replace-with-first-matching-regexp (rest patterns) input)))
+    input))
+
 (expectations
   (desc "nil when no matching extension")
   (expect nil (tff-replace-extension '(("cpp" "h")) "test.rb"))
   (desc "not nil when a extension matches")
   (expect "test.yaml" (tff-replace-extension '(("cpp" "h")("rb" "yaml")) "test.rb"))
-  )
+
+  (desc "replace with first matching regexp")
+  (expect "/some/path/src/test" (tff-replace-with-first-matching-regexp '(("include" "src")("src" "include")) "/some/path/include/test"))
+  (desc "no replacement when no regexp matches")
+  (expect "/some/path/include/test" (tff-replace-with-first-matching-regexp '(("abc" "def")) "/some/path/include/test"))
+ )
